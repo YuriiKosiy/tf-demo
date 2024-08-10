65c72a3da49a6fb8cd2f6ed747cd7b6752912730
Yurii Kosyi <y.kosiy@seosmart.ua>
2024-08-10 15:06:11 +0300

    some changes

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
 module "gke_cluster" {
-  source         = "github.com/YuriiKosiy/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
+  source           = "github.com/YuriiKosiy/tf-google-gke-cluster"
+  GOOGLE_REGION    = var.GOOGLE_REGION
+  GOOGLE_PROJECT   = var.GOOGLE_PROJECT
+  GKE_MACHINE_TYPE = var.GKE_MACHINE_TYPE
+  GKE_NUM_NODES    = var.GKE_NUM_NODES
+  GKE_CLUSTER_NAME = var.GKE_CLUSTER_NAME
+  GKE_POOL_NAME    = var.GKE_POOL_NAME
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "p72-demo"
+    prefix = "terraform/state"
+  }
 }
--- vars.tfvars
+++ vars.tfvars
@@ -1,3 +1,4 @@
 GOOGLE_PROJECT   = "p72-demo"
 GOOGLE_REGION    = "europe-west1-b"
 GKE_CLUSTER_NAME = "p72-demo"
+GKE_POOL_NAME    = "p72-demo-pool"
