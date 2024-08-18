a8f7b06ecefaaca4b6db79d00ea064343a8d7cad
Yurii Kosyi <y.kosiy@seosmart.ua>
2024-08-18 15:56:01 +0300

    start without flux_bootstrap

--- main.tf
+++ main.tf
@@ -17,14 +17,6 @@ module "gke_cluster" {
   GKE_POOL_NAME    = var.GKE_POOL_NAME
 }
 
-module "flux_bootstrap" {
-  source            = "github.com/YuriiKosiy/tf-fluxcd-flux-bootstrap"
-  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
-  private_key       = module.tls_private_key.private_key_pem
-  config_path       = module.gke_cluster.kubeconfig
-  github_token      = var.GITHUB_TOKEN
-}
-
 module "tls_private_key" {
   source    = "github.com/YuriiKosiy/tf-hashicorp-tls-keys"
   algorithm = "RSA"
